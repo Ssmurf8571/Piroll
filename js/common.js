b06d703fbd0b8783276f7654e646485f27cb6dfa
Ssmurf8571 <mastervkirilla@gmail.com>
2021-02-21 21:18:23 +0300

    Remmoved scripts from home.html to common.js

--- js/common.js
+++ js/common.js
@@ -7,3 +7,35 @@ $(window).on('load', function() {
 	$(".loader").delay(400).fadeOut("slow");
 
 });
+
+
+
+$( "#login_btn" ).click(function() {
+
+	document.getElementById("id01").style.display = "block";
+
+
+	  // Get the modal
+	  var modal = document.getElementById('id01');
+  
+	  // When the user clicks anywhere outside of the modal, close it
+	  window.onclick = function(event) {
+		  if (event.target == modal) {
+			  modal.style.display = "none";
+		  }
+	  }
+});
+
+
+
+
+$( "#close" ).click(function() {
+
+	document.getElementById("id01").style.display = "none";
+
+
+  $(".cancelbtn").click(function() {
+    document.getElementById("id01").style.display = "none";
+  });
+
+});
